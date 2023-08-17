f40bb390aaddfba6188ebf1c4bf16534eb67c71f
rm88430 <rm88430@fiap.com.br>
2023-08-16 22:30:50 -0300

    segundo commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+const index = require('./routers/index')
+
+app.use('/', index)
 
 module.exports = app;
-a;
